1c135b2bf913f11364e4f7d18d1a92fab28b34c8
AaronKurowski <aaron12342@att.net>
2021-11-25 10:52:58 -0600

    Updated readme and base file structure added with connection to mysql db

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # GradeEmAll
+
+Track student progress and administer grading based on chosen criteria
---
+++ gradeManual.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>Grading Home</title>
+    
+        <style>
+            h1 {
+                text-align: center;
+            }
+        </style>
+    </head>
+
+    <body>
+        <h1>Grading Home</h1>
+    </body>
+</html>
